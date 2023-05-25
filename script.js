edf7a57eebf5e5214838c28d76002556dbfb235b
Çağatay Sunal <cagataysunal9199@gmail.com>
2023-05-25 23:43:35 +0300

    Center the canvas and add button

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-const bigBox = document.querySelector("#container");
+const bigBox = document.querySelector("#canvas");
 
 for (let i = 0; i < 16; i++) {
 
-	let row = document.createElement("div");
-	row.classList.add("row");
+    let row = document.createElement("div");
+    row.classList.add("row");
 
-	for (let j = 0; j < 16; j++) {
-		let smallBox = document.createElement("div");	
-		smallBox.classList.add("col");
-		row.append(smallBox);
-	}
+    for (let j = 0; j < 16; j++) {
+        let smallBox = document.createElement("div");    
+        smallBox.classList.add("col");
+        row.append(smallBox);
+    }
 
-	bigBox.append(row);
+    bigBox.append(row);
 }
 
 const boxes = document.getElementsByClassName("col");
 
 for (let i = 0; i < boxes.length; i++) {
-	boxes[i].addEventListener("mouseover", function(e) {
-		console.log(e);
-		// if (this.classList.contains("drawn") === true) {
-		// 	this.classList.remove("drawn");
-		// } else {
-		// 	this.classList.add("drawn");
-		// }
-		this.classList.add("drawn");
-	});
+    boxes[i].addEventListener("mouseover", function(e) {
+        console.log(e);
+        this.classList.add("drawn");
+    });
 }
