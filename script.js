a504af00380b36c4fb26f21cf9bd766d8dc8d14b
Çağatay Sunal <cagataysunal9199@gmail.com>
2023-05-27 22:16:52 +0300

    Add user prompt

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const bigBox = document.querySelector("#canvas");
-
-for (let i = 0; i < 16; i++) {
-
-    let row = document.createElement("div");
-    row.classList.add("row");
-
-    for (let j = 0; j < 16; j++) {
-        let smallBox = document.createElement("div");    
-        smallBox.classList.add("col");
-        row.append(smallBox);
-    }
-
-    bigBox.append(row);
-}
-
 const boxes = document.getElementsByClassName("col");
 
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("mouseover", function(e) {
-        console.log(e);
-        this.classList.add("drawn");
-    });
+function createBox(sideLength) {
+    bigBox.textContent = "";
+
+    for (let i = 0; i < sideLength; i++) {
+
+        let row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let j = 0; j < sideLength; j++) {
+            let smallBox = document.createElement("div");    
+            smallBox.classList.add("col");
+            row.append(smallBox);
+        }
+
+        bigBox.append(row);
+    }
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseover", function(e) {
+            console.log(e);
+            this.classList.add("drawn");
+        });
+    }
 }
+
+createBox(16);
+
+
+
+const boxButton = document.querySelector("#reset-canvas");
+
+boxButton.addEventListener("click", function(e) {
+    let number = +prompt("How many squares per side?");
+    number = number > 100 ? 100 : number;
+    createBox(number);
+});
